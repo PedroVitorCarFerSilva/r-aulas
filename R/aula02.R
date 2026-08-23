765a0c3f38d8734cc3e567763d4bff22bf4491dd
PedroVitorCarFerSilva <pv10072006@gmail.com>
2026-08-23 06:54:11 -0300

    alteracoes para facilitar acesso ao github

--- R/aula02.R
+++ R/aula02.R
@@ -19,7 +19,7 @@ setwd("Dados/Limpos")
 setwd("../..")
 setwd("..")
 # Definir diretório do projeto
-projeto_dir <- "C:/Users/Usuario/Downloads/R_Aulas"
+projeto_dir <- "C:/Users/Usuario/Downloads/r-aulas"
 setwd(projeto_dir)
 # Importar dados em csv
 ?read.csv
---
+++ R/gitacesso.R
@@ -0,0 +1,9 @@
+#################
+# GITHUB ACESSO #
+#################
+
+install.packages("usethis")
+credentials::set_github_pat()
+gitcreds::gitcreds_get()
+
+# $ git clone https://github.com/PedroVitorCarFerSilva/r-aulas.git
